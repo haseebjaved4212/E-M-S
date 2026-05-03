a2cd9021bde7da44aea0ac6ac8cd7ca51080e45a
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-05-03 20:33:06 +0500

    feat: implement App layout with Navbar and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar/Navbar";
-
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <>
-    <Navbar/>
+    <Navbar/> 
+    <div>This is Main App</div>
+    <Footer/>
     </>
   )
 }
