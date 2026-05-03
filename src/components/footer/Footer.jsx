ed1ef1b3319ca6e9f3d373b24abbbcc6b4460fd8
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-05-03 20:41:21 +0500

    feat: add Layout and Footer components to base project structure

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-base-100 text-base-content p-4 border-t-2 border-gray-200">
+    <footer className="footer sm:footer-horizontal footer-center bg-base-100 text-base-content p-4 border-t border-gray-200">
   <aside>
     <p>Copyright © {new Date().getFullYear()} - All right reserved by E-M-S</p>
   </aside>
